e6f7ed60a94006bb8f614e6402c73159f7346edf
David Smith <djsmith611@me.com>
2024-02-16 16:26:08 -0700

    monthly cost now updates with insertion and deletion of employees

--- client.js
+++ client.js
@@ -17,6 +17,7 @@ const totalMonthly = document.getElementById('totalMonthly');
 // Employee currentIndex
 var currentIndex = 0;
 
+// Monthly cost to update
 var currentMonthlyCost = 0;
 
 /**Function to add employees to the DOM.
@@ -26,52 +27,44 @@ var currentMonthlyCost = 0;
 function addEmployee(event){
     // Incrementing currentIndex
     currentIndex ++;
-
     // Creating a new employee row
     let employee = document.createElement('tr');
-
     // Creating a new index table data
     let index = document.createElement('td');
     index.textContent = currentIndex;
     employee.appendChild(index);
-
     // Creating a new first name table data
     let firstName = document.createElement('td');
     firstName.textContent = firstNameInput.value;
     employee.appendChild(firstName);
-
     // Creating a new last name table data
     let lastName = document.createElement('td');
     lastName.textContent = lastNameInput.value;
     employee.appendChild(lastName);
-
     // Creating a new id number table data
     let idNumber = document.createElement('td');
     idNumber.textContent = idInput.value;
     employee.appendChild(idNumber);
-
     let jobTitle = document.createElement('td');
     jobTitle.textContent = jobTitleInput.value;
     employee.appendChild(jobTitle);
-
     // Creating a new salary table data
     let salary = document.createElement('td');
     salary.textContent = salaryInput.value;
     salary.className = 'salaryValue';
     employee.appendChild(salary);
-
-    // Calling update on monthly cost
-    updateMonthlyCost();
-
+    // Calling update on monthly cost, false for no deletion
+    updateMonthlyCost(false, parseFloat(salaryInput.value));
     // Creating a new remove button
     let removeButton = document.createElement('button'); //will add onclick attribute after writing function
     removeButton.textContent = 'Delete';
     removeButton.className = 'deleteButton';
     removeButton.addEventListener('click', removeEmployee);
     employee.appendChild(removeButton);
-
     // Appending employee to the DOM
     employees.appendChild(employee);
+    // Clearing all input fields
+    clearInputs();
 }
 
 
@@ -80,28 +73,40 @@ function addEmployee(event){
  */
 
 
-/**Function to delete employees
- * Will call update monthly cost, update index
+/**
+ * Removes employee and updates the salary
  * @param {click} event 
  */
 function removeEmployee(event){
+    // Selecting this employee's salary
+    let salaryElement = event.target.closest('tr').querySelector('.salaryValue');
+    // Parsing employee's salary into a number
+    let yearlyPay = parseFloat(salaryElement.textContent);
+    // Updating monthly cost, true for deletion
+    updateMonthlyCost(true, yearlyPay);
+    // Removing employee from list
     event.target.parentElement.remove();
 }
 
 
-/**Function to update monthly cost
- * Will call check if over budget
- * @param {click} event 
+/**
+ * Updates the monthly cost variable and DOM representation
+ * @param {boolean} isDeletion *makes function reuseable for adding and deleting employees
+ * @param {number} salaryValue 
  */
-function updateMonthlyCost(event){
-    let salaryValue = salaryInput.value;
-    
-    // Dividing by 12 for monthly cost
-    let monthlyPay = salaryValue / 12;
-    currentMonthlyCost += monthlyPay;
-
-    // Updating monthly cost on DOM
-    totalMonthly.textContent = currentMonthlyCost;
+function updateMonthlyCost(isDeletion = false, salaryValue = 0){
+    // Translating the yearly salary to monthly pay
+    let monthlyPay = salaryValue/12;
+    // Checking if function is being called as a deletion
+    if(isDeletion){
+        // If removing an employee
+        currentMonthlyCost -= monthlyPay;
+    } else {
+        // If adding an employee
+        currentMonthlyCost += monthlyPay;
+    }
+    // Updating totalMonthly, using toFixed to prevent 'weird computer math'
+    totalMonthly.textContent = currentMonthlyCost.toFixed(2);
 }
 
 
@@ -109,14 +114,20 @@ function updateMonthlyCost(event){
  * 
  */
 
-
-/* Event listeners to give buttons functionality */
-submitButton.addEventListener('click', function(event){
-    event.preventDefault();
-    addEmployee();
+/**
+ * Clears input fields
+ * @param {click} event 
+ */
+function clearInputs(event){
     firstNameInput.value = '';
     lastNameInput.value = '';
     idInput.value = '';
     jobTitleInput.value = '';
     salaryInput.value = '';
+}
+
+/* Event listeners to give buttons functionality */
+submitButton.addEventListener('click', function(event){
+    event.preventDefault();
+    addEmployee();
 })
