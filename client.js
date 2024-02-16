8349f83fb601bf8a27cf130f0d1917ebe92e10d4
David Smith <djsmith611@me.com>
2024-02-15 20:45:08 -0700

    ran npm install and npm audit fix. added necessary data-testid tags. Added pseudocode outline to js file to guide the code creation of the project.

---
+++ client.js
@@ -0,0 +1,16 @@
+// Global variables and constants
+
+// Function to add employees to the DOM, button needs to have data-testid="submitButton".
+// Will call update monthly cost and over budget check 
+
+// Function to check if over budget and apply over-budget if neccesary
+
+// Function to delete employees
+// Will call update monthly cost, update index
+
+// Function to update monthly cost
+// Will call check if over budget
+
+// Function to update employeeIndex
+
+// Event listeners to give buttons functionality
