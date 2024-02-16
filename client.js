1cdb9fecd933344fe5657b43736922d82af29b46
David Smith <djsmith611@me.com>
2024-02-16 14:55:28 -0700

    added document variables in JavaScript. Updated function notes.

--- client.js
+++ client.js
@@ -1,16 +1,39 @@
-// Global variables and constants
+/* Global variables and constants */
+// Input constants
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+const idInput = document.getElementById('idNumber');
+const salaryInput = document.getElementById('salary');
 
-// Function to add employees to the DOM, button needs to have data-testid="submitButton".
-// Will call update monthly cost and over budget check 
+// Employee container
+const employees = document.getElementById('employeeData');
 
-// Function to check if over budget and apply over-budget if neccesary
+// Monthly cost display
+const totalMonthly = document.getElementById('totalMonthly');
 
-// Function to delete employees
-// Will call update monthly cost, update index
+/**Function to add employees to the DOM, button needs to have data-testid="submitButton".
+ * Will call update monthly cost and over budget check
+ */
 
-// Function to update monthly cost
-// Will call check if over budget
 
-// Function to update employeeIndex
+/**Function to check if over budget and apply over-budget if neccesary
+ * 
+ */
 
-// Event listeners to give buttons functionality
+
+/**Function to delete employees
+ * Will call update monthly cost, update index
+ */
+
+
+/**Function to update monthly cost
+ * Will call check if over budget
+ */
+
+
+/**Function to update employeeIndex
+ * 
+ */
+
+
+/* Event listeners to give buttons functionality */
